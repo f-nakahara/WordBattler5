979474c18d528cf2376296bf9dc8ab5207ae4f64
nakahara <meshiprio503@gmail.com>
2020-05-03 17:38:18 +0900

    インプットフォームテスト

--- src/js/component/Play/Form.js
+++ src/js/component/Play/Form.js
@@ -1,12 +1,56 @@
 import React from "react"
 
 class Form extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            "keyword": ""
+        }
+    }
+
+    componentDidMount() {
+        this.focusAttackInput()
+    }
+
+    doAttack(e) {
+        e.preventDefault()
+        const keyword = this.state.keyword
+        this.setState({
+            "keyword": ""
+        })
+    }
+
+    changeKeyword(e) {
+        const keyword = e.target.value
+        this.setState({
+            "keyword": keyword
+        })
+    }
+
+    focusAttackInput() {
+        const element = document.getElementById("keywordInput")
+        element.focus()
+    }
+
     render() {
         return (
             <div>
                 <form className="form-inline">
-                    <input className="form-control w-75"></input>
-                    <input type="submit" value="送信" className="btn btn-danger w-25"></input>
+                    <input
+                        id="keywordInput"
+                        value={this.state.keyword}
+                        className="form-control w-75"
+                        onChange={this.changeKeyword.bind(this)}
+                    >
+
+                    </input>
+                    <input
+                        type="submit"
+                        value="送信"
+                        className="btn btn-danger w-25"
+                        onClick={this.doAttack.bind(this)}
+                    >
+                    </input>
                 </form>
             </div>
         )
